28bffc1d6ca2b550f8ee4676b0e16f6a4cc064f9
Yuhui Zhang <yz4317@nyu.edu>
2020-10-11 15:25:21 -0400

    Added IB outlet

--- ChessBeta/ViewController.swift
+++ ChessBeta/ViewController.swift
@@ -9,6 +9,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var b64: UIImageView!
+    @IBOutlet weak var b63: UIImageView!
+    @IBOutlet weak var b62: UIImageView!
+    @IBOutlet weak var b61: UIImageView!
+    @IBOutlet weak var b60: UIImageView!
+    @IBOutlet weak var b59: UIImageView!
+    @IBOutlet weak var b58: UIImageView!
+    @IBOutlet weak var b57: UIImageView!
+    @IBOutlet weak var col8: UIStackView!
+    @IBOutlet weak var b56: UIImageView!
+    @IBOutlet weak var b55: UIImageView!
+    @IBOutlet weak var b54: UIImageView!
+    @IBOutlet weak var b53: UIImageView!
+    @IBOutlet weak var b52: UIImageView!
+    @IBOutlet weak var b51: UIImageView!
+    @IBOutlet weak var b50: UIImageView!
+    @IBOutlet weak var b49: UIImageView!
+    @IBOutlet weak var col7: UIStackView!
+    @IBOutlet weak var b48: UIImageView!
+    @IBOutlet weak var b47: UIImageView!
+    @IBOutlet weak var b46: UIImageView!
+    @IBOutlet weak var b45: UIImageView!
+    @IBOutlet weak var b44: UIImageView!
+    @IBOutlet weak var b43: UIImageView!
+    @IBOutlet weak var b42: UIImageView!
+    @IBOutlet weak var b41: UIImageView!
+    @IBOutlet weak var col6: UIStackView!
+    @IBOutlet weak var b40: UIImageView!
+    @IBOutlet weak var b39: UIImageView!
+    @IBOutlet weak var b38: UIImageView!
+    @IBOutlet weak var b37: UIImageView!
+    @IBOutlet weak var b36: UIImageView!
+    @IBOutlet weak var b35: UIImageView!
+    @IBOutlet weak var b34: UIImageView!
+    @IBOutlet weak var b33: UIImageView!
+    @IBOutlet weak var col5: UIStackView!
+    @IBOutlet weak var b32: UIImageView!
+    @IBOutlet weak var b31: UIImageView!
+    @IBOutlet weak var b30: UIImageView!
+    @IBOutlet weak var b29: UIImageView!
+    @IBOutlet weak var b28: UIImageView!
+    @IBOutlet weak var b27: UIImageView!
+    @IBOutlet weak var b26: UIImageView!
+    @IBOutlet weak var b25: UIImageView!
+    @IBOutlet weak var col4: UIStackView!
+    @IBOutlet weak var b24: UIImageView!
+    @IBOutlet weak var b23: UIImageView!
+    @IBOutlet weak var b22: UIImageView!
+    @IBOutlet weak var b21: UIImageView!
+    @IBOutlet weak var b20: UIImageView!
+    @IBOutlet weak var b19: UIImageView!
+    @IBOutlet weak var b18: UIImageView!
+    @IBOutlet weak var b17: UIImageView!
+    @IBOutlet weak var col3: UIStackView!
+    @IBOutlet weak var b16: UIImageView!
+    @IBOutlet weak var b15: UIImageView!
+    @IBOutlet weak var b14: UIImageView!
+    @IBOutlet weak var b13: UIImageView!
+    @IBOutlet weak var b12: UIImageView!
+    @IBOutlet weak var b11: UIImageView!
+    @IBOutlet weak var b10: UIImageView!
+    @IBOutlet weak var b9: UIImageView!
+    @IBOutlet weak var col2: UIStackView!
+    @IBOutlet weak var b8: UIImageView!
+    @IBOutlet weak var b7: UIImageView!
+    @IBOutlet weak var b6: UIImageView!
+    @IBOutlet weak var b5: UIImageView!
+    @IBOutlet weak var b4: UIImageView!
+    @IBOutlet weak var b3: UIImageView!
+    @IBOutlet weak var b2: UIImageView!
+    @IBOutlet weak var col1: UIStackView!
+    @IBOutlet weak var b1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
